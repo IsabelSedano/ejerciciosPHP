78f3692ed64eea438ee2d4754322ed924105d5b0
Isabel Sedano <isedanolop@gmail.com>
2016-10-24 18:34:51 +0200

    commit4

--- soluciones/Ejercicio12/Recepcion.php
+++ soluciones/Ejercicio12/Recepcion.php
@@ -5,30 +5,30 @@ $nombre = request.getParameter("nombre");
             $cookies = request.getCookies();
             $nuevaCookie = null;
             if ($cookies != null){
-                for (Cookie cookie: cookies){ 
-                    if (cookie.getName().equals("nombre")){ //busca la cookie 'nombre'
-                        nuevaCookie = cookie; //si existe cookie, la guardas
-                    }
+                foreach($_COOKIE as $nombre => $valor){ //recorre el array
+                    if ($cookie.getName().equals("nombre")){ //busca la cookie que se llame "nombre"
+                    $recordado = $cookie.getValue();  //guardas el texto de la cookie en 'recordado'
                 }
             }
-            if (nuevaCookie != null) { //comprueba si la cookie ya existia, si es la 'vieja'
-                nuevaCookie.setValue(nombre); //si existia la cookie, le cambia el nombre
-            } else {
-                nuevaCookie = new Cookie("nombre", nombre); //sino la crea
             }
-            nuevaCookie.setMaxAge(120);
+            if ($nuevaCookie != null) { //comprueba si la cookie ya existia, si es la 'vieja'
+                $nuevaCookie.setValue(nombre); //si existia la cookie, le cambia el nombre
+            } else {
+                $nuevaCookie = new Cookie("nombre", nombre); //sino la crea
+            }
+            $nuevaCookie.setMaxAge(120);
             response.addCookie(nuevaCookie);  //envia la cookie
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Servlet Recepcion</title>        
+    <title>Servlet Recepcion</title>        
 </head>
-<body>
-<h1>Datos</h1>
-Nombre: " + nombre + <br>
-Contraseña: " + password + <br>
-<p><a href='Formulario'>  Volver </a></p>
-</body>
+    <body>
+        <h1>Datos</h1>
+        Nombre: " + nombre + <br>
+        Contraseña: " + password + <br>
+        <p><a href='Formulario'>  Volver </a></p>
+    </body>
 </html>
